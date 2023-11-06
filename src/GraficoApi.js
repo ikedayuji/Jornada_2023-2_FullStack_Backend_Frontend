96edb761f99f7d122b5e7e2ae72700abf9e5df81
ikedayuji <93358246+ikedayuji@users.noreply.github.com>
2023-11-06 17:57:30 -0300

    Jornada v7.1

--- src/GraficoApi.js
+++ src/GraficoApi.js
@@ -15,7 +15,7 @@ function Grafico() {
         trigger: 'axis',
       },
       legend: {
-        data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine'],
+        data: ['Moegas', 'Secador de Agregados', 'Sistema de Pesagem', 'Misturador', 'Silo de Armazenamento', 'Sistema de Alimentação de Asfalto', 'Sistema de Abastecimento de Combustível', 'Sistema de Aditivos', 'Elevador', 'Esteira', 'Silo de Armazenamento de Mistura'],
       },
       grid: {
         left: '3%',
@@ -31,41 +31,71 @@ function Grafico() {
       xAxis: {
         type: 'category',
         boundaryGap: false,
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        data: ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom'],
       },
       yAxis: {
         type: 'value',
       },
       series: [
         {
-          name: 'Email',
+          name: 'Moegas',
           type: 'line',
           stack: 'Total',
           data: [120, 132, 101, 134, 90, 230, 210],
         },
         {
-          name: 'Union Ads',
+          name: 'Secador de Agregados',
           type: 'line',
           stack: 'Total',
           data: [220, 182, 191, 234, 290, 330, 310],
         },
         {
-          name: 'Video Ads',
+          name: 'Sistema de Pesagem',
           type: 'line',
           stack: 'Total',
           data: [150, 232, 201, 154, 190, 330, 410],
         },
         {
-          name: 'Direct',
+          name: 'Misturador',
           type: 'line',
           stack: 'Total',
           data: [320, 332, 301, 334, 390, 330, 320],
         },
         {
-          name: 'Search Engine',
+          name: 'Silo de Armazenamento de Mistura',
           type: 'line',
           stack: 'Total',
           data: [820, 932, 901, 934, 1290, 1330, 1320],
+        },
+        {
+          name: 'Sistema de Alimentação de Asfalto',
+          type: 'line',
+          stack: 'Total',
+          data: [850, 232, 601, 534, 990, 1430, 820],
+        },
+        {
+          name: 'Sistema de Abastecimento de Combustível',
+          type: 'line',
+          stack: 'Total',
+          data: [120, 832, 401, 234, 550, 730, 1120],
+        },
+        {
+          name: 'Sistema de Aditivos',
+          type: 'line',
+          stack: 'Total',
+          data: [600, 732, 621, 544, 690, 730, 920],
+        },
+        {
+          name: 'Elevador',
+          type: 'line',
+          stack: 'Total',
+          data: [620, 222, 561, 824, 690, 930, 424],
+        },
+        {
+          name: 'Esteira',
+          type: 'line',
+          stack: 'Total',
+          data: [659, 888, 1561, 259, 900, 1430, 420],
         },
       ],
     };
